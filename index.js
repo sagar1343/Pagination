f41cdc87ea9bae3706895919ce5c303e6c81312d
Sagar <sagarchakrawarti24@gmail.com>
2024-01-23 18:23:49 +0530

    fix bug in pagination

--- index.js
+++ index.js
@@ -7,40 +7,64 @@ async function getPost() {
 }
 const posts = getPost();
 let postStartIndex = 0;
-let postEndIndex = postStartIndex + 5;
-
+let currentPage = 1;
 const listGroup = document.querySelector(".list-group");
-function updatePost() {
+function updatePost(postStartIndex) {
   posts.then((posts) => {
-    posts.slice(postStartIndex, postEndIndex).forEach((post) => {
+    posts.slice(postStartIndex, postStartIndex + 5).forEach((post) => {
       const list = document.createElement("li");
       list.innerHTML = `<p>${post.id} ${post.title}</p>`;
       listGroup.appendChild(list);
     });
   });
 }
-updatePost();
+updatePost(postStartIndex);
 
 const prev = document.querySelector("#prev");
 const next = document.querySelector("#next");
 
 next.addEventListener("click", function () {
-  if (postEndIndex <= 100) {
+  if (postStartIndex < 95) {
     listGroup.innerHTML = "";
     postStartIndex += 5;
-    postEndIndex += 5;
-    updatePost();
-  } else {
-    listGroup.innerHTML = `<h1>No more posts</h1>`;
+    currentPage++;
+    updatePost(postStartIndex);
+    pagination();
   }
 });
 prev.addEventListener("click", function () {
-  if (postStartIndex <= 100) {
+  if (postStartIndex > 0) {
     listGroup.innerHTML = "";
     postStartIndex -= 5;
-    postEndIndex -= 5;
-    updatePost();
-  } else {
-    listGroup.innerHTML = `<h1>No more posts</h1>`;
+    currentPage--;
+    updatePost(postStartIndex);
+    pagination();
+  }
+});
+const pages = document.querySelector(".pages");
+function pagination() {
+  pages.innerHTML = "";
+  posts.then((posts) => {
+    for (let index = 1; index <= posts.length / 5; index++) {
+      const page = document.createElement("li");
+      if (currentPage == index) {
+        page.classList.add("active");
+      }
+      page.innerHTML = `${index}`;
+      pages.appendChild(page);
+    }
+  });
+}
+
+pagination();
+
+pages.addEventListener("click", function (event) {
+  if (event.target.tagName === "LI") {
+    const n = event.target.innerHTML;
+    postStartIndex = 5 * n - 5;
+    listGroup.innerHTML = "";
+    currentPage = n;
+    updatePost(postStartIndex);
+    pagination();
   }
 });
