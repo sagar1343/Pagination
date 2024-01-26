fb4cf8fbdfd8784ca4a75c857c8abb9ad5f35ed8
Sagar <sagarchakrawarti24@gmail.com>
2024-01-26 18:17:11 +0530

    improve pagination layout

--- index.js
+++ index.js
@@ -1,70 +1,59 @@
+const totalPostPerPage = 5;
+const listGroup = document.querySelector(".list-group");
+const currentPageBtn = document.getElementById("currentPage");
+const prevPageBtn = document.getElementById("prev");
+const nextPageBtn = document.getElementById("next");
+let n = 1; //n---> pageNumber
 async function getPost() {
   const url = "https://jsonplaceholder.typicode.com/posts";
   const data = await fetch(url);
-  const jsonData = await data.json();
-  console.log(jsonData);
-  return jsonData;
+  const posts = await data.json();
+  console.log(posts);
+  const totalPost = posts.length;
+  return [posts, totalPost];
 }
-const posts = getPost();
-let postStartIndex = 0;
-let currentPage = 1;
-const listGroup = document.querySelector(".list-group");
-function updatePost(postStartIndex) {
-  posts.then((posts) => {
-    posts.slice(postStartIndex, postStartIndex + 5).forEach((post) => {
+async function setUp() {
+  const [posts, totalPost] = await getPost();
+  const totalPages = totalPost / totalPostPerPage;
+  function getPage(n) {
+    listGroup.innerHTML = "";
+    currentPageBtn.innerText = n;
+    if (n === 1) {
+      prevPageBtn.classList.add("none");
+    } else {
+      prevPageBtn.classList.remove("none");
+      prevPageBtn.innerText = n - 1;
+    }
+    if (n === totalPages) {
+      nextPageBtn.classList.add("none");
+    } else {
+      nextPageBtn.classList.remove("none");
+      nextPageBtn.innerText = n + 1;
+    }
+
+    let An = (n - 1) * totalPostPerPage;
+    posts.slice(An, An + totalPostPerPage).forEach((item) => {
       const list = document.createElement("li");
-      list.innerHTML = `<p>${post.id} ${post.title}</p>`;
+      list.innerHTML = `<p>${item.id} ${item.title}</p>`;
       listGroup.appendChild(list);
     });
+  }
+
+  getPage(1);
+
+  currentPageBtn.innerText = n;
+  prevPageBtn.addEventListener("click", () => {
+    n--;
+    getPage(n);
   });
-}
-updatePost(postStartIndex);
-
-const prev = document.querySelector("#prev");
-const next = document.querySelector("#next");
-
-next.addEventListener("click", function () {
-  if (postStartIndex < 95) {
-    listGroup.innerHTML = "";
-    postStartIndex += 5;
-    currentPage++;
-    updatePost(postStartIndex);
-    pagination();
-  }
-});
-prev.addEventListener("click", function () {
-  if (postStartIndex > 0) {
-    listGroup.innerHTML = "";
-    postStartIndex -= 5;
-    currentPage--;
-    updatePost(postStartIndex);
-    pagination();
-  }
-});
-const pages = document.querySelector(".pages");
-function pagination() {
-  pages.innerHTML = "";
-  posts.then((posts) => {
-    for (let index = 1; index <= posts.length / 5; index++) {
-      const page = document.createElement("li");
-      if (currentPage == index) {
-        page.classList.add("active");
-      }
-      page.innerHTML = `${index}`;
-      pages.appendChild(page);
-    }
+  nextPageBtn.addEventListener("click", () => {
+    n++;
+    getPage(n);
   });
+
+  const pageStart = document.getElementById("pageStart");
+  const pageEnd = document.getElementById("pageEnd");
+  pageStart.addEventListener("click", () => getPage(1));
+  pageEnd.addEventListener("click", () => getPage(totalPages));
 }
-
-pagination();
-
-pages.addEventListener("click", function (event) {
-  if (event.target.tagName === "LI") {
-    const n = event.target.innerHTML;
-    postStartIndex = 5 * n - 5;
-    listGroup.innerHTML = "";
-    currentPage = n;
-    updatePost(postStartIndex);
-    pagination();
-  }
-});
+setUp();
